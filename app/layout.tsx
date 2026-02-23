778c221e1f3750f59f4f8b475989c8a4159d42c9
Isaac <isaac@podshrink.com>
2026-02-23 16:23:59 -0500

    feat: add Google Analytics 4 (G-K0Q8WMZZJB)

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K0Q8WMZZJB"></script>
+        <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-K0Q8WMZZJB');` }} />
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <ScrollToTop />
